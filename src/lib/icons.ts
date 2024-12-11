dc4817b9c8c50abbadd95da43770095e007290a8
Santiago Cézar <santiagocezar2013@gmail.com>
2024-12-11 12:27:45 -0300

    broke that icon

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,6 +1,29 @@
-import { icons } from '@iconify-json/mdi'
-import { getIcons } from '@iconify/utils'
+import { icons as mdiIcons } from '@iconify-json/mdi'
+import { getIcons, iconToSVG, expandIconSet } from '@iconify/utils'
 
-function generateSymbols(names: string[]) {
-    getIcons(icons, names)
+export function generateSymbols(names: string[]) {
+    expandIconSet(mdiIcons)
+
+    const { icons = {}, not_found = [], prefix } = getIcons(mdiIcons, names, true) ?? {}
+
+
+    if (not_found.length)
+        console.warn(`some icons were not found: ${not_found}`)
+
+    console.log(names)
+
+    let symbols = ""
+
+    for (const icon in icons) {
+        const { body, attributes } = iconToSVG(icons[icon]!)
+        console.log(icons[icon]!)
+        console.log(attributes)
+
+        symbols += `
+            <symbol id="${prefix}:${icon}" viewBox="${attributes.viewBox}">
+                ${body}
+            </symbol>
+        `
+    }
+    return symbols
 }
