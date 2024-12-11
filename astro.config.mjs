f8e3b04e4844f0b31709a97f294ca6efaebcac59
Santiago Cézar <santiagocezar2013@gmail.com>
2024-12-11 17:00:36 -0300

    why did i have that set

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,11 +10,6 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    build: {
-      sourcemap: "inline",
-    }
-  },
   integrations: [
     solidJs(),
     svelte(),
