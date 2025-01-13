ffefddee05f78ffd699081cadc4b1e83ee9aba87
Santiago Cézar <santiagocezar2013@gmail.com>
2025-01-13 11:45:48 -0300

    s/less/scss/ & simpler layouts

---
+++ src/game.ts
@@ -0,0 +1,25 @@
+export const name = "Monopolio"
+export const showHeader = false;
+
+export const iconPack = "material-symbols"
+export const iconPrefix = "ic"
+export const icons = [
+    "arrow-back",
+    "chevron-right",
+    "share",
+    "remove",
+    "add",
+    "delete",
+    "upload",
+    "download",
+    "account-balance",
+    "payments",
+    "house",
+    "home-work",
+    "check",
+    "directions-railway",
+    "swap-horiz",
+]
+
+
+export { default as View } from './mono/View.svelte'
